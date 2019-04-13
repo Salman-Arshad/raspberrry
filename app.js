3d71a8ad684b38bcff5818771604937ea912fb20
Your Name <you@example.com>
2019-04-13 12:05:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const app = require("express")();
+const bodyparse = require('body-parser');
+app.use(bodyparse.json());
+app.use(bodyparse.urlencoded({extended: true}));
+var mysql = require('mysql');
+app.listen(5000,(error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("server listenimg");
+    }
+    
+});
+app.get("/",(req,res)=>{
+    var data=[]
+    conn.query("select * from readings",(err,resu)=>{
+        resu.forEach(element => {
+            data.push(element.data)
+        });
+        res.send(data)
+    })
+})
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tester',
+  });
+  conn.connect(err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('connected to zybase');
+  });
+app.post("/",(req,res)=>{
+    res.send(req.body.data);
+    console.log(req.body)
+})
+ 
